c23a9dde0921a816cf595fd436d9879f2e7acbca
Xiaochen Fan <fan.daybreak@gmail.com>
2019-03-17 23:43:18 +0800

    add backend server

---
+++ backend/swapper.py
@@ -0,0 +1,19 @@
+from image_engine import BingImageEngine
+
+
+def request_images(item):
+    key = "f956df8f4aa74af396fbc1a5072e7960"
+    bie = BingImageEngine(key)
+    bie.request(item)
+
+def swap(src_img, tgt_img):
+    pass
+
+def compare(src_img, tgt_img):
+    pass
+
+def process(src_img, item):
+    pass
+
+
+
