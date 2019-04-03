f304c5dd8974d28a7c8cdc2d76cb1dd14fca6374
Xiaochen Fan <fan.daybreak@gmail.com>
2019-04-03 23:35:29 +0800

    Flask used in backend

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from image_engine import BingImageEngine
 from swapper import *
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+swapper = Swapper()
+swapper.start_img_engine()
+#swapper.start_fs_engine()
+
+
+@app.route("/", methods=["POST"])
+def swap():
+    image = request.json["image"]
+    item = request.json["item"]
+    swapper.set_image(image)
+    swapper.request_images(item)
+
+    results = list()
+    for idx in range(swapper.img_engine.length()):
+        pass
+     #   swapped_img = swapper.process_one(idx)
+     #   if not swapped_img:
+     #       results.append(swapped_img)
+
+    return jsonify({
+        "status": "OK",
+        "result": results
+    })
 
 if __name__ == "__main__":
     app.run()
--- backend/swapper.py
+++ backend/swapper.py
@@ -1,7 +1,6 @@
 from image_engine import BingImageEngine
 import face_swap_py as fspy
 
-# Initialize face swap
 landmarks_path = 'data/shape_predictor_68_face_landmarks.dat'
 model_3dmm_h5_path = 'data/BaselFaceModel_mod_wForehead_noEars.h5'
 model_3dmm_dat_path = 'data/BaselFace.dat'
@@ -15,39 +14,43 @@ with_expr = False
 with_gpu = False
 gpu_device_id = 0
 
-# faceswap engine
-fs = fspy.FaceSwap(landmarks_path, model_3dmm_h5_path,
-            model_3dmm_dat_path, reg_model_path,
-            reg_deploy_path, reg_mean_path,
-            seg_model_path, seg_deploy_path,
-            generic, with_expr, with_gpu, gpu_device_id)
-
 
 class Swapper:
     def __init__(self):
         self.src_img = None
         self.cur_tgt = None
+        self.fs_engine = None
+        self.img_engine = None
+
+    def start_fs_engine(self):
+        self.fs_engine = fspy.FaceSwap(landmarks_path, model_3dmm_h5_path,
+                        model_3dmm_dat_path, reg_model_path,
+                        reg_deploy_path, reg_mean_path,
+                        seg_model_path, seg_deploy_path,
+                        generic, with_expr, with_gpu, gpu_device_id)
+    
+    def start_img_engine(self):
         key = "f956df8f4aa74af396fbc1a5072e7960"
-        self.engine = BingImageEngine(key)
+        self.img_engine = BingImageEngine(key)
     
     def set_image(self, img):
         self.src_img = fspy.FaceData(img)
 
     def request_images(self, item):
-        self.engine.request(item)
+        self.img_engine.request(item)
 
     def swap(self):
         swapped = self.src_img
         if self.src_img and self.cur_tgt:
         # invoke faceswapping (self.src_img, tgt_img) 
-            swapped = fs.transfer(self.src_img, self.cur_tgt)
+            swapped = self.fs_engine.transfer(self.src_img, self.cur_tgt)
         return swapped
 
     def compare(self, idx):
-        tgt_img = self.engine.iterate(idx)
+        tgt_img = self.img_engine.iterate(idx)
         self.cur_tgt = fspy.FaceData(tgt_img)
         # invoke faceswapping (self.src_img, tgt_img) 
-        ret = fs.compare(self.src_img, self.cur_tgt)
+        ret = self.fs_engine.compare(self.src_img, self.cur_tgt)
         return ret
 
     def process_one(self, idx):
@@ -57,11 +60,11 @@ class Swapper:
         return output
 
     def process(self, img, item):
-        self.src_img = img
+        self.set_image(img)
         if not self.request_images(item):
             print("Cannot search for images!")
 
-        for idx in range(self.engine.length()):
+        for idx in range(self.img_engine.length()):
             self.process_one(idx)
 
 
