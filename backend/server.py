7de658f6050a7d0dffb762897f53bd1ebc90f703
Xiaochen Fan <fan.daybreak@gmail.com>
2019-04-05 00:23:31 +0800

    Add frontend code

--- backend/server.py
+++ backend/server.py
@@ -9,12 +9,24 @@ swapper = Swapper()
 swapper.start_img_engine()
 #swapper.start_fs_engine()
 
+@app.route("/item", methods=["POST"])
+def item():
+    print(request.json)
+    item = request.json["item"]
+
+@app.route("/image", methods=["POST"])
+def image():
+    img = request.files.get('image')
+    return jsonify({
+        "status": "OK",
+    })
 
 @app.route("/", methods=["POST"])
 def swap():
+    print(request.json)
     image = request.json["image"]
     item = request.json["item"]
-    swapper.set_image(image)
+    #swapper.set_image(image)
     swapper.request_images(item)
 
     results = list()
