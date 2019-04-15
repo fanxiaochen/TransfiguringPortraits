c347f23f79a587f7b71fbb632494dcc30d569079
Xiaochen Fan <fan.daybreak@gmail.com>
2019-04-15 23:28:02 +0800

    Struggling with react native

---
+++ frontend/faceswap/App.js
@@ -0,0 +1,119 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import axios from 'axios';
+import ImagePicker from 'react-native-image-picker';
+
+
+const serverUrl = 'http://localhost:5000';
+const http = axios.create({
+  baseURL: serverUrl
+});
+
+const options = {
+  title: 'Select Avatar',
+  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+
+// open camera
+ImagePicker.launchCamera(options, (response) => {
+  if (response.didCancel) {
+    console.log('User cancelled image picker');
+  } else if (response.error) {
+    console.log('ImagePicker Error: ', response.error);
+  } else if (response.customButton) {
+    console.log('User tapped custom button: ', response.customButton);
+  } else {
+    const source = { uri: response.uri };
+ 
+    // You can also display the image using data:
+    // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+ 
+    this.setState({
+      avatarSource: source,
+    });
+  }
+});
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  onImageSend(){
+    let body = new FormData();
+    imagePath = '/home/xiaochen/Workspace/TransfiguringPortraits';
+    body.append('photo', {uri: imagePath,name: 'xi1',filename :'xi1.png',type: 'image/png'});
+    body.append('Content-Type', 'image/png');
+
+    fetch(serverUrl+'/image',{ method: 'POST',headers:{  
+        "Content-Type": "multipart/form-data",
+        "otherHeader": "foo",
+        } , body :body} )
+      .then((res) => checkStatus(res))
+      .then((res) => res.json())
+      .then((res) => { console.log("response" +JSON.stringify(res)); })
+      .catch((e) => console.log(e))
+      .done()
+  }
+
+  onItemSend(){
+    http.post('/item',{
+      // image
+      // item
+    });
+
+  }
+
+  onMessageSend(){
+    http.post('/',{
+      // image
+      // item
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
