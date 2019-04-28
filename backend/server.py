e911d4058276a9cb2b40d11faace9a69d0663848
Xiaochen Fan <fan.daybreak@gmail.com>
2019-04-28 23:16:03 +0800

    Show image from app

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 import threading
 import time
 import os
-from flask import Flask, request, jsonify, send_file, url_for
+from flask import Flask, request, jsonify, send_file, url_for, Response
 import numpy as np
 import cv2
 from image_engine import BingImageEngine
@@ -19,6 +19,13 @@ swapped_idx = 0
 image_cache = 'static'
 if not os.path.exists(image_cache):
     os.mkdir(image_cache)
+
+@app.route("/test", methods=["GET"])
+def test():
+    # send image back
+    path = 'home/xiaochen/Workspace/TransfiguringPortraits/backend/static/0.jpg'
+    resp = Response(open(path, 'rb'), mimetype="image/jpeg")
+    return resp
 
 @app.route("/swapped", methods=["GET"])
 def has_swapped():
@@ -40,11 +47,15 @@ def swapped_images():
     if img_idx < len(swapped_list):
         #img_url = os.path.join(request.url_root, )
         re_url = url_for('static', filename=swapped_list[img_idx])
+        #re_url = url_for('static', filename='0.jpg')
         print(request.url_root)
         img_url = os.path.join(request.url_root, re_url)
         print(img_url)
-        img_url = request.url_root + re_url
+        img_url = request.url_root + re_url[1:]
         print(img_url)
+      #  path = '/home/xiaochen/Workspace/TransfiguringPortraits/backend/static/0.jpg'
+      #  resp = Response(open(path, 'rb'), mimetype="image/jpeg")
+      #  return resp
         return jsonify({
         "status": "Success",
         "url": img_url
@@ -130,3 +141,4 @@ def swap():
 if __name__ == "__main__":
      #app.run(debug=True, host= '0.0.0.0')
      app.run(host='192.168.31.126')
+     #app.run(host='127.0.0.1',port=4949, debug=True)
--- frontend/faceswap/src/swap.js
+++ frontend/faceswap/src/swap.js
@@ -4,6 +4,7 @@ import {
     Text,
     Button,
     Image,
+    Alert,
     StyleSheet
 } from 'react-native'
 
@@ -41,6 +42,14 @@ export default class Swap extends Component {
           }
           else {
             // text wait a while
+            Alert.alert(
+              'Next Image Not Ready',
+              'please wait a while',
+              [
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+              ],
+              {cancelable: false},
+            );
           }
         });
     }
@@ -52,6 +61,7 @@ export default class Swap extends Component {
             <Image
               style={{width: 50, height: 50}}
               source={{uri: this.state.cur_url}}
+              //source={{uri: "http://192.168.31.126:5000/static/0.jpg"}}
             />
             <Button
                 style={styles.button}
