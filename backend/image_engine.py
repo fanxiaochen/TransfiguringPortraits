c740c201debf74ba3c75f3ef05ae922eaac58b29
Xiaochen Fan <fan.daybreak@gmail.com>
2019-05-06 23:41:08 +0800

    Add multi-threads for image engine

--- backend/image_engine.py
+++ backend/image_engine.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import threading
 import numpy
 from azure.cognitiveservices.search.imagesearch import ImageSearchAPI
 from msrest.authentication import CognitiveServicesCredentials
@@ -22,15 +23,33 @@ class BingImageEngine(ImageEngine):
     def request(self, item):
         image_results = self.client.images.search(query=item)
         images = list()
+        images = None 
         if image_results.value:
             print("Total number of images returned: {}".format(len(image_results.value)))
             self.msg = "Successfully requested!"
-            for i in range(int(len(image_results.value)/3)):
-                image_result = image_results.value[i]
+
+            images = [None]* len(image_results.value)
+            def get_image(idx, list, results):
+                image_result = results[idx]
                 print("image thumbnail url: {}".format(image_result.thumbnail_url))
                 print("image content url: {}".format(image_result.content_url))
                 img = Image.open(urlopen(image_result.thumbnail_url))
-                images.append(cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR))
+                #images.append(cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR))
+                list[idx] = cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR)
+
+            for i in range(len(image_results.value)):
+                x = threading.Thread(target=get_image, args=(i, images, image_results.value))
+                x.start()
+                x.join()
+
+
+
+          #  for i in range(int(len(image_results.value)/3)):
+          #      image_result = image_results.value[i]
+          #      print("image thumbnail url: {}".format(image_result.thumbnail_url))
+          #      print("image content url: {}".format(image_result.content_url))
+          #      img = Image.open(urlopen(image_result.thumbnail_url))
+          #      images.append(cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR))
         else:
             self.msg = "No image results returned!"
             print("No image results returned!")
